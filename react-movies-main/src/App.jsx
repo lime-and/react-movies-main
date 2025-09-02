4f7d2f663c6cd2e8b4b8a9c6d0ff2d53fca62ddd
lime <michaelliman21@gmail.com>
2025-09-02 23:11:58 +0900

    api call change

--- react-movies-main/src/App.jsx
+++ react-movies-main/src/App.jsx
@@ -7,7 +7,7 @@ import { getTrendingMovies, updateSearchCount } from './appwrite.js'
 
 const API_BASE_URL = 'https://api.themoviedb.org/3';
 
-const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+const apiKey = import.meta.env.VITE_TMDB_API_KEY;
 
 // Check if API key is available
 if (!API_KEY) {
